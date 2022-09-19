4b99527a5c102675ff3924a10ac81e756c454d88
吴富其 <1090323359@qq.com>
2022-09-19 11:43:29 +0800

    更改UI更新帧，提高动画效率

--- wukonganimation/src/main/java/com/wukonganimation/tween/TweenAnimationHandler.kt
+++ wukonganimation/src/main/java/com/wukonganimation/tween/TweenAnimationHandler.kt
@@ -1,21 +1,47 @@
 package com.wukonganimation.tween
 
-import android.animation.ValueAnimator
-import androidx.core.animation.addListener
 
-class TweenAnimationHandler {
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.os.SystemClock
+import android.view.Choreographer
+import java.util.*
 
+class TweenAnimationHandler(var mTweenManager: TweenManager) {
 
+    private var lastTime: Long = 0
+
+    private var isDestroy = false
 
     @Volatile
-    private var startTime: Long = 0
+    private var isStart = false
 
-    private var mTweenManager: TweenManager
+    private val mDestroyMessage = Object()
 
-    private var mValueAnimator: ValueAnimator? = null
+    private val mDestroyWhat = 9999
 
-    constructor(tweenManager: TweenManager) {
-        mTweenManager = tweenManager
+    private val mHandler = Handler(Looper.getMainLooper()) {
+        if (it.what == mDestroyWhat && it.obj == mDestroyMessage) {
+            //为了让isDestroy  在主线程执行，避免2个线程争抢资源
+            isDestroy = true
+        }
+        return@Handler false
+    }
+
+    private val doFrame = object : Choreographer.FrameCallback {
+        override fun doFrame(frameTimeNanos: Long) {
+            val frameTime = SystemClock.uptimeMillis()
+            val dt = frameTime - lastTime
+            lastTime = frameTime
+            if (this@TweenAnimationHandler.mTweenManager.isEmpty() || isDestroy) {
+                isStart = false
+                return
+            }
+            Choreographer.getInstance().postFrameCallback(this)
+            update(dt)
+        }
+
     }
 
     private fun update(dt: Long) {
@@ -23,31 +49,26 @@ class TweenAnimationHandler {
     }
 
     fun start() {
-
-        if (mValueAnimator != null) {
+        if (isStart) {
             return
         }
-        mValueAnimator = ValueAnimator.ofInt(0)
-        mValueAnimator?.addListener {
-            mValueAnimator = null
-            start()
-        }
-        mValueAnimator?.duration = Long.MAX_VALUE
-        mValueAnimator?.addUpdateListener {
-            if (mTweenManager.isEmpty()) {
-                mValueAnimator?.cancel()
-                mValueAnimator = null
-                startTime = 0
-                return@addUpdateListener
+        isStart = true
+        mHandler.post {
+            //保证在主线程执行
+            if (isDestroy) {
+                return@post
             }
-            update(it.currentPlayTime - startTime)
-            startTime = it.currentPlayTime
+            lastTime = SystemClock.uptimeMillis()
+            Choreographer.getInstance().postFrameCallback(doFrame)
         }
-        mValueAnimator?.start()
     }
 
     fun destroy() {
-        mValueAnimator?.cancel()
-        mValueAnimator = null
+        isStart = false
+        val message = Message()
+        message.what = mDestroyWhat
+        message.obj = mDestroyMessage
+        //把销毁消息放到链表最前面，保证能第一个执行销毁消息。
+        mHandler.sendMessageAtTime(message, 0)
     }
 }
